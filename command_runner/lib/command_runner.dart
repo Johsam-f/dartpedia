6f949c86711e747ab14b24553fa3020a8a07c179
Johsam <johsamfcress@gmail.com>
2026-06-28 15:47:44 +0200

    add error handling and colorful output support

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -1,10 +1,9 @@
 /// A simple command runner to handle command-line arguments.
-///
-/// More extensive documentation for this library goes here.
+
 library;
 
 export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
 export 'src/help_command.dart';
-
-// TODO: Export any other libraries intended for clients of this package.
+export 'src/exceptions.dart';
+export 'src/console.dart';
